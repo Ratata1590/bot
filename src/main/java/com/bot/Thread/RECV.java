e8c38aba06f4bfe6c5de5d65c07225e819070d60
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-06-13 14:59:27 +0700

    update serverBot

--- src/main/java/com/bot/Thread/RECV.java
+++ src/main/java/com/bot/Thread/RECV.java
@@ -6,27 +6,27 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class RECV extends LinkAbstract {
-	public void run() {
-		while (!Thread.currentThread().isInterrupted()) {
-			HttpClient client = HttpClientBuilder.create().build();
-			HttpGet get = new HttpGet(this.proxyUrl.concat("/socketHandler"));
-			get.addHeader("sockRestId", sessionId);
-			try {
-				HttpResponse response;
-				int ctry = retry;
-				do {
-					response = client.execute(get);
-					ctry--;
-					if (ctry == 0) {
-						throw new Exception("out of retry");
-					}
-				} while (response.getStatusLine().getStatusCode() != 200);
-				response.getEntity().writeTo(sock.getOutputStream());
-				sock.getOutputStream().flush();
-			} catch (Exception e) {
-				disconnectRemoteSocket();
-				break;
-			}
-		}
-	}
+  public void run() {
+    while (!Thread.currentThread().isInterrupted()) {
+      HttpClient client = HttpClientBuilder.create().build();
+      HttpGet get = new HttpGet(this.proxyUrl.concat("/socketHandler"));
+      get.addHeader("sockRestId", sessionId);
+      try {
+        HttpResponse response;
+        int ctry = retry;
+        do {
+          response = client.execute(get);
+          ctry--;
+          if (ctry == 0) {
+            throw new Exception("out of retry");
+          }
+        } while (response.getStatusLine().getStatusCode() != 200);
+        response.getEntity().writeTo(sock.getOutputStream());
+        sock.getOutputStream().flush();
+      } catch (Exception e) {
+        disconnectRemoteSocket();
+        break;
+      }
+    }
+  }
 }
--- src/main/java/com/bot/Thread/ServerBot.java
+++ src/main/java/com/bot/Thread/ServerBot.java
@@ -14,94 +14,116 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class ServerBot extends Thread {
-	private String proxyUrl;
-	private String botName;
+  private String proxyUrl;
+  private String botName;
 
-	public void run() {
-		while (!Thread.currentThread().isInterrupted()) {
-			try {
-				String sockRestId = botAddConnection(botName);
-				connectToDestination(sockRestId);
-			} catch (Exception e) {
-			}
-		}
-	}
+  public void run() {
+    try {
+      botInit(botName);
+    } catch (Exception e1) {
+      e1.printStackTrace();
+      return;
+    }
+    while (!Thread.currentThread().isInterrupted()) {
+      try {
+        String sockRestId = botAddConnection(botName);
+        connectToDestination(sockRestId);
+      } catch (Exception e) {
+      }
+    }
+  }
 
-	public void startServer(String proxyUrl, String botName) throws Exception {
-		this.proxyUrl = proxyUrl;
-		this.botName = botName;
-		start();
-	}
+  public void startServer(String proxyUrl, String botName) throws Exception {
+    this.proxyUrl = proxyUrl;
+    this.botName = botName;
+    start();
+  }
 
-	private String botAddConnection(String botName) throws Exception {
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(proxyUrl.concat("/botAddConnection"));
-		post.addHeader("botName", this.botName);
-		HttpResponse response;
-		do {
-			response = client.execute(post);
-		} while (response.getStatusLine().getStatusCode() != 200);
-		return EntityUtils.toString(response.getEntity());
-	}
+  private void botInit(String botName) throws Exception {
+    HttpClient client = HttpClientBuilder.create().build();
+    HttpPost post = new HttpPost(proxyUrl.concat("/botInit"));
+    post.addHeader("botName", this.botName);
+    HttpResponse response;
+    do {
+      response = client.execute(post);
+    } while (response.getStatusLine().getStatusCode() != 200);
+  }
 
-	private void connectToDestination(String sockRestId) {
-		Socket sock = null;
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpGet get = new HttpGet(this.proxyUrl.concat("/mirror").concat("/socketHandler"));
-		get.addHeader("sockRestId", sockRestId);
-		try {
-			HttpResponse response;
-			do {
-				response = client.execute(get);
-			} while (response.getStatusLine().getStatusCode() != 200);
-			String command = EntityUtils.toString(response.getEntity());
-			sock = openSocket(command);
-			SEND send = new SEND();
-			send.startSend(proxyUrl.concat("/mirror"), sock, sockRestId);
-			RECV recv = new RECV();
-			recv.startSend(proxyUrl.concat("/mirror"), sock, sockRestId);
-		} catch (Exception e) {
-			disconnectRemoteSocket(sockRestId);
-			try {
-				sock.close();
-			} catch (Exception e2) {
-			}
-		}
-	}
+  private String botAddConnection(String botName) throws Exception {
+    HttpClient client = HttpClientBuilder.create().build();
+    HttpPost post = new HttpPost(proxyUrl.concat("/botAddConnection"));
+    post.addHeader("botName", this.botName);
+    HttpResponse response;
+    do {
+      response = client.execute(post);
+    } while (response.getStatusLine().getStatusCode() != 200);
+    return EntityUtils.toString(response.getEntity());
+  }
 
-	private void disconnectRemoteSocket(String sockRestId) {
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(proxyUrl.concat("/mirror/socketControl/disconnect"));
-		try {
-			HttpResponse response;
-			int ctry = 5;
-			do {
-				post.setHeader("sessionId", sockRestId);
-				response = client.execute(post);
-				ctry--;
-				if (ctry == 0) {
-					throw new Exception("out of retry");
-				}
-			} while (response.getStatusLine().getStatusCode() != 200);
-		} catch (Exception e) {
-		}
-	}
+  private void connectToDestination(String sockRestId) {
+    Socket sock = null;
+    HttpClient client = HttpClientBuilder.create().build();
+    HttpGet get = new HttpGet(this.proxyUrl.concat("/mirror/socketHandler"));
+    get.addHeader("sockRestId", sockRestId);
+    try {
+      String command = "";
+      HttpResponse response = null;
+      while (command.equals("")) {
+        response = client.execute(get);
+        if (response.getStatusLine().getStatusCode() != 200) {
+          return;
+        }
+        command = EntityUtils.toString(response.getEntity());
+        Thread.sleep(1000);
+      }
 
-	private static Socket openSocket(String command) throws Exception {
-		Socket socket;
-		String[] info = command.split(":");
-		String host = info[0];
-		int port = Integer.valueOf(info[1]);
-		try {
-			InetAddress inteAddress = InetAddress.getByName(host);
-			SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
-			socket = new Socket();
-			int timeoutInMs = 10 * 1000;
-			socket.connect(socketAddress, timeoutInMs);
-			return socket;
-		} catch (SocketTimeoutException ste) {
-			ste.printStackTrace();
-			throw ste;
-		}
-	}
+      sock = openSocket(command);
+      SEND send = new SEND();
+      send.startSend(proxyUrl.concat("/mirror"), sock, sockRestId);
+      RECV recv = new RECV();
+      recv.startSend(proxyUrl.concat("/mirror"), sock, sockRestId);
+    } catch (Exception e) {
+      disconnectRemoteSocket(sockRestId);
+      try {
+        sock.close();
+      } catch (Exception e2) {
+      }
+    }
+  }
+
+  private void disconnectRemoteSocket(String sockRestId) {
+    HttpClient client = HttpClientBuilder.create().build();
+    HttpPost post = new HttpPost(proxyUrl.concat("/mirror/socketControl/disconnect"));
+    try {
+      HttpResponse response;
+      int ctry = 5;
+      do {
+        post.setHeader("sessionId", sockRestId);
+        response = client.execute(post);
+        ctry--;
+        if (ctry == 0) {
+          throw new Exception("out of retry");
+        }
+      } while (response.getStatusLine().getStatusCode() != 200);
+    } catch (Exception e) {
+    }
+  }
+
+  private static Socket openSocket(String command) throws Exception {
+    Socket socket;
+    String[] info = command.split(":");
+    String host = info[0];
+    int port = Integer.valueOf(info[1]);
+    try {
+      InetAddress inteAddress = InetAddress.getByName(host);
+      SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
+      socket = new Socket();
+      int timeoutInMs = 10 * 1000;
+      socket.connect(socketAddress, timeoutInMs);
+      return socket;
+    } catch (SocketTimeoutException ste) {
+      ste.printStackTrace();
+      throw ste;
+    }
+  }
 }
